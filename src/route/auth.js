89a6eff6cae3bd8571f51f0c9fee68e4405d4bf2
Sergii <52467090+SergAdmin@users.noreply.github.com>
2023-09-16 08:35:14 +0300

    finish auth-2

--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -47,6 +53,34 @@ router.get('/singup', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
+
+//=============================
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля не заповнені.",
+    })
+  }
+
+  try {
+    User.create({ email, password, role })
+
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрований.',
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка створення користувача.',
+    })
+  }
+})
+
+//=============================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
